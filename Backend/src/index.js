e7a8a8d00d028f06d1765575dbe221a4139fdab1
Harsh Sharma <harsh1618sharma@gmail.com>
2025-04-09 02:53:47 +0530

    Final Fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -39,6 +39,12 @@ const io = new Server(server, {
   },
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  next();
+});
+
+
 // Middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
